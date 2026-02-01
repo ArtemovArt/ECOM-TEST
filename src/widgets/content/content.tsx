3ed79cd581c574d48ef38a30ae342953e90e1b6b
unknown <aretmov07@mail.ru>
2026-02-01 12:19:15 +0300

    типизировал debounce, добавил фильтрацию по товарам

--- src/widgets/content/content.tsx
+++ src/widgets/content/content.tsx
@@ -1,30 +1,23 @@
+import { useState } from 'react';
+
 import { SearchBlock } from '../../entities/searchBlock/searchBlock';
 import { useGetData } from '../../shared/hooks/useGetData';
 import { CardContainer } from '../cardContainer/cardContainer';
 
 import styles from './content.module.css';
 
-// const cardsArray: CardsArray = [
-//   { id: '1', description: 'Card 1', img: testImg1, isFav: false, price: '12 999', isAddedInCard: false },
-//   { id: '2', description: 'Card 2', img: testImg2, isFav: true, price: '8 999', isAddedInCard: true },
-//   { id: '3', description: 'Card 3', img: testImg1, isFav: false, price: '15 999', isAddedInCard: false },
-//   { id: '4', description: 'Card 4', img: testImg2, isFav: true, price: '7 999', isAddedInCard: true },
-//   { id: '1', description: 'Card 1', img: testImg1, isFav: false, price: '12 999', isAddedInCard: false },
-//   { id: '2', description: 'Card 2', img: testImg2, isFav: true, price: '8 999', isAddedInCard: true },
-//   { id: '3', description: 'Card 3', img: testImg1, isFav: false, price: '15 999', isAddedInCard: false },
-//   { id: '4', description: 'Card 4', img: testImg2, isFav: true, price: '7 999', isAddedInCard: true },
-// ];
 export const Content = () => {
   const { isPending, data, error } = useGetData();
-  console.log('data', data);
+  const [filterQuery, setFilterQuery] = useState('');
+
   if (isPending) return <div>Loading...</div>;
 
   if (error) return <div>Oops..</div>;
 
   return (
     <div className={styles.content}>
-      <SearchBlock />
-      <CardContainer cards={data ?? []} />
+      <SearchBlock setFilterQuery={setFilterQuery} />
+      <CardContainer cards={data ?? []} filterQuery={filterQuery} />
     </div>
   );
 };
